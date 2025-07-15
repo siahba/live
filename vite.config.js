1f6490b15165325c288597a6c30d6352e50d940d
Siahba <josiahbarlow@gmail.com>
2025-07-15 08:47:40 -0600

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/menstrual-tracker/', // change this to your repo name
-});
